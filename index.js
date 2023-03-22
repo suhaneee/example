72ee26b91cb8b2b85a19fdf45378a8ee817e2c7e
suhaneee <suhanee55@gmail.com>
2023-03-22 16:42:28 +0900

    ex

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
+// loadFile 안에 JSON 배열 등록
+const loadFile = async () => {
+  const response = await fetch("./curation.json", {
+    method: "GET",
+  });
 
+  const data = await response.json();
+  console.log(data);
 
-fetch("./curation.json")
-.then((res) => {
-console.log(res.json());
-return res.json()
+  const images = data.forEach((item, i) => {
+    item.image;
+    console.log(i + item.image);
+  });
 
-})
-.then((obj) => {
-    console.log(obj)
-List(obj);
-})
+  
+
+  function setBannerImage() {
+    const bannerImgs = document.querySelectorAll(".banner-img");
+    bannerImgs.forEach((bannerImg, i) => {
+      const imageSrc = data[i].image;
+      bannerImg.setAttribute("src", imageSrc);
+      console.log(bannerImg);
+    });
+  }
+  setBannerImage();
+
+  function setBannerText() {
+    const bannertexts = document.querySelectorAll(".banner-text");
+    bannertexts.forEach((bannertext, i) => {
+      const bannertextKo = data[i].text_ko;
+      const value = eval(bannertextKo);
+      bannertext.innerHTML = value;
+      console.log(bannertext);
+    });
+  }
+
+  setBannerText();
+};
+
+loadFile();
+
+// header footer 분리해서 index.html에 붙히기
+window.addEventListener("load", function () {
+  var allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    var includePath = el.dataset.includePath;
+    if (includePath) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
+  });
+});
